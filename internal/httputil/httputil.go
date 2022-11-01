9a0eaf41c89cba8efadf4ed982ade2157830bdba
Patrik Nordlén <patrik.nordlen@einride.tech>
2022-11-01 23:45:32 +0100

    fix: better auth header check

--- internal/httputil/httputil.go
+++ internal/httputil/httputil.go
@@ -32,19 +32,19 @@ func RequireToken(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := context.Background()
 		authHeader := strings.Split(r.Header.Get("Authorization"), " ")
-		if len(authHeader) != 2 {
-			ReturnUnauthorized(w)
-		} else {
+		if len(authHeader) == 2 && authHeader[0] == "Bearer" {
 			idToken := authHeader[1]
 			parsedToken, err := idtoken.Validate(ctx, idToken, "")
 
 			if err != nil {
-				fmt.Println("Error: ", err)
+				log.Println("Error: ", err)
 				ReturnUnauthorized(w)
 			} else {
 				r.Header.Add("user-email", parsedToken.Claims["email"].(string))
 				next(w, r)
 			}
+		} else {
+			ReturnUnauthorized(w)
 		}
 	}
 }
