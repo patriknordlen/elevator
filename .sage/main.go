0b34c1bbc20e1f21f15aa8ebe6d7c3c758ac634c
Patrik Nordlén <patrik.nordlen@einride.tech>
2023-10-13 10:21:43 +0200

    chore: wip

--- .sage/main.go
+++ .sage/main.go
@@ -54,7 +54,7 @@ func GoLint(ctx context.Context) error {
 
 func GoLicenses(ctx context.Context) error {
 	sg.Logger(ctx).Println("checking Go licenses...")
-	return sggolicenses.Check(ctx)
+	return sggolicenses.CheckDir(ctx, sg.FromGitRoot("."))
 }
 
 func FormatMarkdown(ctx context.Context) error {
